4f0467d5db44fdfcf055af107e22f3fb7beebead
guvanch07 <guvanchamanov777@gmail.com>
2022-06-02 23:25:12 +0300

    worked with IBAction and IBLabel

--- storyboard_test_app/ViewController.swift
+++ storyboard_test_app/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet var labelcollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+   
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        print("onPressed")
+        label.text = "hello label"
+        
+    }
+    
 }
 
